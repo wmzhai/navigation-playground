2330e1f6a374553a0fd4510de4b19f05acc6c968
Weiming Zhai <wmzhai@gmail.com>
2017-05-04 18:11:20 +0800

    add StacksInTabs

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ import CustomTabs from './custom_tabs'
 import Drawer from './drawer'
 import TabsInDrawer from './tabs_in_drawer'
 import ModalStack from './modal_stack'
+import StacksInTabs from './stacks_in_tabs'
 
 class App extends React.Component {
   render() {
