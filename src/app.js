0d15f19c0c4e6d2a76cc5fa52dc67981877e0ac8
Weiming Zhai <wmzhai@gmail.com>
2017-05-04 18:17:23 +0800

    add real app

--- src/app.js
+++ src/app.js
@@ -19,24 +19,125 @@ import StacksOverTabs from './stacks_over_tabs'
 import SimpleStack from './simple_stack'
 import SimpleTabs from './simple_tabs'
 
-
-class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
-      </View>
-    )
+const ExampleRoutes = {
+  SimpleStack: {
+    name: 'Stack Example',
+    description: 'A card stack',
+    screen: SimpleStack
+  },
+  SimpleTabs: {
+    name: 'Tabs Example',
+    description: 'Tabs following platform conventions',
+    screen: SimpleTabs
+  },
+  Drawer: {
+    name: 'Drawer Example',
+    description: 'Android-style drawer navigation',
+    screen: Drawer
+  },
+  TabsInDrawer: {
+    name: 'Drawer + Tabs Example',
+    description: 'A drawer combined with tabs',
+    screen: TabsInDrawer
+  },
+  CustomTabs: {
+    name: 'Custom Tabs',
+    description: 'Custom tabs with tab router',
+    screen: CustomTabs
+  },
+  ModalStack: {
+    name: Platform.OS === 'ios' ? 'Modal Stack Example' : 'Stack with Dynamic Header',
+    description: Platform.OS === 'ios' ? 'Stack navigation with modals' : 'Dynamically showing and hiding the header',
+    screen: ModalStack
+  },
+  StacksInTabs: {
+    name: 'Stacks in Tabs',
+    description: 'Nested stack navigation in tabs',
+    screen: StacksInTabs
+  },
+  StacksOverTabs: {
+    name: 'Stacks over Tabs',
+    description: 'Nested stack navigation that pushes on top of tabs',
+    screen: StacksOverTabs
+  },
+  LinkStack: {
+    name: 'Link in Stack',
+    description: 'Deep linking into a route in stack',
+    screen: SimpleStack,
+    path: 'people/Jordan'
+  },
+  LinkTabs: {
+    name: 'Link to Settings Tab',
+    description: 'Deep linking into a route in tab',
+    screen: SimpleTabs,
+    path: 'settings'
   }
 }
+
+
+const MainScreen = ({ navigation }) => (
+  <ScrollView>
+    <Banner />
+    {Object.keys(ExampleRoutes).map((routeName) =>
+      <TouchableOpacity
+        key={routeName}
+        onPress={() => {
+          const { path, params, screen } = ExampleRoutes[routeName];
+          const { router } = screen;
+          const action = path && router.getActionForPathAndParams(path, params);
+          navigation.navigate(routeName, {}, action);
+        }}
+      >
+        <View style={styles.item}>
+          <Text style={styles.title}>{ExampleRoutes[routeName].name}</Text>
+          <Text style={styles.description}>{ExampleRoutes[routeName].description}</Text>
+        </View>
+      </TouchableOpacity>
+    )}
+  </ScrollView>
+)
+
+
+const AppNavigator = StackNavigator({
+  ...ExampleRoutes,
+  Index: {
+    screen: MainScreen
+  }
+}, {
+  initialRouteName: 'Index',
+  headerMode: 'none',
+
+  /*
+   * Use modal on iOS because the card mode comes from the right,
+   * which conflicts with the drawer example gesture
+   */
+  mode: Platform.OS === 'ios' ? 'modal' : 'card'
+})
 
 const styles = {
-  container: {
-    flex: 1,
+  item: {
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: '#ddd',
+  },
+  image: {
+    width: 120,
+    height: 120,
+    alignSelf: 'center',
+    marginBottom: 20,
+    resizeMode: 'contain',
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#444',
+  },
+  description: {
+    fontSize: 13,
+    color: '#999',
+  },
 }
 
-export default App
+export default () => <AppNavigator />
