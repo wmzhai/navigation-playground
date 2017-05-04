27355be74764358fae15a04a04ec882ff0c259a8
Weiming Zhai <wmzhai@gmail.com>
2017-05-04 17:33:36 +0800

    init

---
+++ main.js
@@ -0,0 +1,24 @@
+import Expo from 'expo';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up main.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}
+
+Expo.registerRootComponent(App);
